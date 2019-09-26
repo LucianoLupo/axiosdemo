74464c72f8e9f8986b8dd13aafa436dd61d14067
Luciano <lucianolupo97@gmail.com>
2019-09-26 12:01:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './App.module.css';
+import Comp1 from './components/comp1/Comp1';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Comp1/>
 
+    );
+}
 export default App;
+
---
+++ src/components/comp1/Comp1.js
@@ -0,0 +1,100 @@
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from './comp1.module.css';
+function Comp1() {
+
+  const [data, setData] = useState([]);
+  const [filteredNames, setFilteredNames] = useState([])
+  const [query, setQuery] = useState(1);
+  const [url, setUrl] = useState(
+    'https://reqres.in/api/users?page=1',
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getPersent = (selected, total) => {
+    return Math.floor(selected * 100 / total);
+  }
+
+  useEffect( () => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data.data);
+        setFilteredNames(result.data.data.filter( user => user.first_name.length + user.last_name.length > 12 ))
+        setIsLoading(false);
+
+      } catch (error) {
+        setIsError(true);
+        setIsLoading(false);
+
+      }
+      } 
+    fetchData();
+  },[url]);
+
+
+
+  return (
+    <div className={styles.container1}>
+    {
+        <div style={{margin:'0 auto'}}>
+            <input
+            type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)}
+        />
+        <button
+            type="button"
+            onClick={() =>
+            setUrl(`https://reqres.in/api/users?page=${query}`)
+            }
+        >
+            Search
+        </button>
+        {isError && <div>Something went wrong ...</div>}
+        {isLoading ? (
+            <div>Loading ...</div>
+        ) : (
+            <span></span>
+        )}
+        </div>
+    }
+      {
+        data.length > 0 ?
+        <>
+        <div className={styles.listContainer}>
+        <div className={styles.listTotal}>
+        <h2>Lista Total:</h2>
+
+        {data.map(item => (
+                <h3 key={item.id}>
+                {item.first_name} {item.last_name}
+                </h3>
+            ))}
+        </div>
+        <div className={styles.listFiltered}>
+        <h2>Lista filtrada:</h2>
+          {filteredNames.map(item => (
+            <h3 key={item.id}>
+              {item.first_name} {item.last_name}          
+            </h3>
+          ))}
+        </div>
+        </div>
+        <div className={styles.resultContainer}>
+          <h3>El porcentaje de personas cuyo nombre + apellido tienen mas de 12 caracteres es:</h3>
+          <h1> {getPersent(filteredNames.length, data.length)} % </h1>
+        </div>
+
+        </>
+        : <p style={{textAlign:'center'}}> Nothing found</p>
+      }
+    </div>
+  );
+}
+export default Comp1;
+
